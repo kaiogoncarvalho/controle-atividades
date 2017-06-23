7998efc281ba769d7c3aa36dc56a4d082e5047b6
kaiogoncarvalho@gmail.com <kaiogoncarvalho@gmail.com>
2017-06-23 00:55:58 -0300

    Inclusão de nova linguagem

---
+++ resources/lang/pt-br/reset.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Senha.',
+    'email_address' => 'Endereço de E-mail',
+    'reset_password' => 'Resetar a senha',
+    'confirm_password' => 'Confirmar Senha',
+
+];
